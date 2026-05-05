fec4aa0dd494419cb817adf4e925bb2120e0049e
Ben Halpern <ben@dev.to>
2026-05-05 10:41:43 -0400

    Adjust timeout logic for latest migration

--- db/migrate/20260430190002_add_index_on_page_views_article_and_created_at.rb
+++ db/migrate/20260430190002_add_index_on_page_views_article_and_created_at.rb
@@ -5,14 +5,20 @@ class AddIndexOnPageViewsArticleAndCreatedAt < ActiveRecord::Migration[7.0]
   # which queries by article_id and groups by created_at date.
   def up
     safety_assured do
-      execute "SET statement_timeout = 0;"
+      db_user = connection.query_value("SELECT current_user")
+      begin
+        execute "ALTER ROLE #{db_user} SET statement_timeout = 0;"
+        execute "SET statement_timeout = 0;"
 
-      remove_index :page_views, name: "index_page_views_on_article_id_and_created_at", if_exists: true, algorithm: :concurrently
+        remove_index :page_views, name: "index_page_views_on_article_id_and_created_at", if_exists: true, algorithm: :concurrently
 
-      add_index :page_views,
-                %i[article_id created_at],
-                name: "index_page_views_on_article_id_and_created_at",
-                algorithm: :concurrently
+        add_index :page_views,
+                  %i[article_id created_at],
+                  name: "index_page_views_on_article_id_and_created_at",
+                  algorithm: :concurrently
+      ensure
+        execute "ALTER ROLE #{db_user} RESET statement_timeout;"
+      end
     end
   end
 
